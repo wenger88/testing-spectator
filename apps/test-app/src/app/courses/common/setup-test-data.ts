6df24fcd0856ba46915d038efd86358bcc590fd8
Goran <pavlovskigoran88@gmail.com>
2020-02-23 22:25:43 +0100

    presentational tests

--- apps/test-app/src/app/courses/common/setup-test-data.ts
+++ apps/test-app/src/app/courses/common/setup-test-data.ts
@@ -1,6 +1,6 @@
 import {Course} from '../model/course';
-import {COURSES} from '../../../../server/db-data';
 import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
+import { COURSES } from '../../../../../server/db-data';
 
 
 export function setupCourses() : Course[] {
--- apps/test-app/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ apps/test-app/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,27 +1,49 @@
-import { TestBed } from '@angular/core/testing';
 import { CoursesCardListComponent } from './courses-card-list.component';
 import { CoursesModule } from '../courses.module';
 import { createTestComponentFactory, Spectator } from '@netbasal/spectator';
+import { APP_BASE_HREF } from '@angular/common';
+import { setupCourses } from '../common/setup-test-data';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('CoursesCardListComponent', () => {
   let spectator: Spectator<CoursesCardListComponent>;
   const createComponent = createTestComponentFactory({
-    component: CoursesCardListComponent
+    component: CoursesCardListComponent,
+    imports: [CoursesModule],
+    declareComponent: false,
+    providers: [
+      {
+        provide: APP_BASE_HREF,
+        useValue: '/',
+      },
+    ],
   });
   beforeEach(() => {
     spectator = createComponent();
-    spectator.detectChanges();
   });
 
   it('should create the component', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  xit('should display the course list', () => {
-    pending();
+  it('should display the course list', () => {
+    spectator.component.courses = setupCourses();
+    spectator.detectChanges();
+    const cards = spectator.debugElement.queryAll(By.css('.course-card'));
+    expect(cards).toBeTruthy();
+    expect(cards.length).toBe(12);
   });
 
-  xit('should display the first course', () => {
-    pending();
+  it('should display the first course', () => {
+    spectator.component.courses = setupCourses();
+    spectator.detectChanges();
+    const course = spectator.component.courses[0];
+    const card: DebugElement = spectator.debugElement.query(By.css('.course-card:first-child'));
+    expect(card).toBeTruthy();
+    const title: HTMLElement = card.query(By.css('.mat-card-title')).nativeElement;
+    expect(title.textContent).toBe(course.titles.description);
+    const image: HTMLImageElement = card.query(By.css('img')).nativeElement;
+    expect(image.src).toBe(course.iconUrl);
   });
 });
