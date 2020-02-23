c1fb09cd3cd5dd0d9f454e5b544354fbd25ec372
Goran <pavlovskigoran88@gmail.com>
2020-02-23 20:48:57 +0100

    add some data

--- apps/test-app/src/app/app.module.ts
+++ apps/test-app/src/app/app.module.ts
@@ -3,11 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoursesModule } from './courses/courses.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    CoursesModule,
+    AppRoutingModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
